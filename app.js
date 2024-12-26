415d583c2dd962ec8346794a27a4e34be3dc94b7
Karthik18v <bittukarthik77@gmail.com>
2024-12-26 11:01:26 +0530

    completed

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3004',  // Specify the allowed origin
+  methods: 'GET,POST',              // Allow only specific HTTP methods (optional)
+  allowedHeaders: 'Content-Type',   // Specify allowed headers (optional)
+};
+
+
+
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 const Task = require("./Model/TaskModel");
 
 const MONGODB_URI =
